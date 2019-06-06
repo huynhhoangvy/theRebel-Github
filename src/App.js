c42fd00806a61bbb43a96fbf7b7e0e33559bf546
huynhhoangvy <huynh.h.vy@gmail.com>
2019-06-06 18:02:07 +0700

    styling rendersearchrepo

--- src/App.js
+++ src/App.js
@@ -1,143 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button, Container } from 'react-bootstrap';
-import Search from './components/Search.js';
-import Footer from './components/Footer.js';
-import RenderRepo from './components/RenderRepo.js';
-import RenderSearchRepo from './components/RenderSearchRepo.js';
-const ReactMarkdown = require('react-markdown')
-
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Container } from "react-bootstrap";
+import Search from "./components/Search.js";
+import Footer from "./components/Footer.js";
+import RenderRepo from "./components/RenderRepo.js";
+import RenderSearchRepo from "./components/RenderSearchRepo.js";
+const ReactMarkdown = require("react-markdown");
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        userName:'',
-        issues: [],
-        listRepo:[],
-        // isIssue:false,
-        searchInput:'',
-        isListRepo:true,
+      userName: "",
+      issues: [],
+      listRepo: [],
+      // isIssue:false,
+      searchInput: "",
+      isListRepo: true
+    };
+  }
 
-    }
-}
+  componentDidMount = () => {};
 
-componentDidMount = () => {
-}
+  updateInputValue = evt => {
+    this.setState({
+      searchInput: evt.target.value
+    });
+  };
 
-updateInputValue=(evt)=> {
-  this.setState({
-    searchInput: evt.target.value
-  });
-}
+  // use for search Repo =>  return only list of "owner/reponame"
+  getSearchRepo = async repoName => {
+    const url = `https://api.github.com/search/repositories?q=${repoName}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    this.setState({
+      listRepo: data.items,
+      isListRepo: true
+    });
+  };
 
-// use for search Repo =>  return only list of "owner/reponame"
-getSearchRepo = async(repoName) => {
-  const url = `https://api.github.com/search/repositories?q=${repoName}`;
-  let response = await fetch(url);
-  let data = await response.json();
-  this.setState({
-    listRepo: data.items,
-    isListRepo:true,
-  });
-};
-
-// when user click on owner/reponame => return list of issues 
-// Issue Title                issues.title
-// Number of the issue || issues.number
-// Owner of the Issue
-// Owner Avatar
-// How long ago the issue was created in a human-friendly format (e.g. 2 days ago)  // .moment
-// Body of the Issue
-// Label - note the color as returned by the API.  ****(some have, some not)
-// State of Issue (Open/Closed). 
-//////////////////////////////
- getRepo = async(fullName) => {
-  const url = `https://api.github.com/repos/${fullName}/issues`;
-  let response = await fetch(url);
-  let data = await response.json();
-  this.setState({
+  // when user click on owner/reponame => return list of issues
+  // Issue Title                issues.title
+  // Number of the issue || issues.number
+  // Owner of the Issue
+  // Owner Avatar
+  // How long ago the issue was created in a human-friendly format (e.g. 2 days ago)  // .moment
+  // Body of the Issue
+  // Label - note the color as returned by the API.  ****(some have, some not)
+  // State of Issue (Open/Closed).
+  //////////////////////////////
+  getRepo = async fullName => {
+    const url = `https://api.github.com/repos/${fullName}/issues`;
+    let response = await fetch(url);
+    let data = await response.json();
+    this.setState({
       issues: data,
-      isListRepo:false,
-  });
-  console.log("get Repo",this.state.issues)
-};
+      isListRepo: false
+    });
+    console.log("get Repo", this.state.issues);
+  };
 
+  // async getUser(name) {
+  //   const url = `https://api.github.com/users/${name}`;
+  //   let response = await fetch(url);
+  //   let data = await response.json();
+  //   this.setState({
+  //       csScores: data.items,
+  //   });
+  // };
 
-// async getUser(name) {
-//   const url = `https://api.github.com/users/${name}`;
-//   let response = await fetch(url);
-//   let data = await response.json();
-//   this.setState({
-//       csScores: data.items,
-//   });
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  render (){
-  return (
-<div className="App h-100">
-<header>
-  <Search 
-          {...this.state}
-          updateInputValue={this.updateInputValue}
-          getSearchRepo={this.getSearchRepo}
-          
+  render() {
+    return (
+      <div className="App h-100">
+        <header>
+          <Search
+            {...this.state}
+            updateInputValue={this.updateInputValue}
+            getSearchRepo={this.getSearchRepo}
           />
         </header>
 
         <Container className="h-100">
-        {this.state.isListRepo && 
-        <RenderSearchRepo 
-        {...this.state}
-        getSearchRepo={this.getSearchRepo}
-        getRepo={this.getRepo}
-
-        />}
-          {!this.state.isListRepo && 
-          <RenderRepo 
-            {...this.state}
-            getSearchRepo={this.getSearchRepo}
-            getRepo={this.getRepo}
-          
-          />
-          }
+          {this.state.isListRepo && (
+            <RenderSearchRepo
+              {...this.state}
+              getSearchRepo={this.getSearchRepo}
+              getRepo={this.getRepo}
+            />
+          )}
+          {!this.state.isListRepo && (
+            <RenderRepo
+              {...this.state}
+              getSearchRepo={this.getSearchRepo}
+              getRepo={this.getRepo}
+            />
+          )}
         </Container>
-      <Footer />
-    </div>
-  );
-}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/RenderSearchRepo.js
+++ src/components/RenderSearchRepo.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import '../App.css';
-import { Button } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 
 
 class RenderSearchRepo extends React.Component {
     renderSearchResults = () => {
-        return this.props.listRepo.map(repo => {
+        return this.props.listRepo.map(({full_name, description, updated_at, language, stargazers_count}) => {
             return (
-                <li><a href="#  " onClick={() => this.props.getRepo(repo.full_name)}>{repo.full_name}</a></li>
+                
+                <Card className="pb-5 w-100">
+                <Card.Header>
+                    <div>
+                        <Row>
+                            <Col className="col-7">
+                                <a href="#  " onClick={() => this.props.getRepo(full_name)}>{full_name}</a>
+                            </Col>
+                            <Col className="col-3">
+                                <span >{language}</span>
+                            </Col>
+                            <Col className="col-2">
+                                <span >★{stargazers_count}</span>
+                            </Col>
+                        </Row>
+                    </div>
+                    
+                    </Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                    <p>
+                        {description}
+                    </p>
+                    <footer className="blockquote-footer">
+                        Updated <cite title="Source Title">{updated_at}</cite>
+                    </footer>
+                    </blockquote>
+                </Card.Body>
+                </Card>
             )
 
 
@@ -15,12 +43,41 @@ class RenderSearchRepo extends React.Component {
     }
     render() {
         return (
-            <div className="row" style={{backgroundColor:"pink"}}>
-                <div className="col-2">Your are looking at repos</div>
-                <div className="col-10" style={{ backgroundColor: "red" }}>
-                    <div>{this.renderSearchResults()}</div>
-                </div>
-            </div>
+            <Row style={{backgroundColor:"gray"}}>
+                <Col className="col-2" style={{backgroundColor: "blue"}}>
+                    Your are looking at repos
+                </Col>
+            <Col className="col-10">
+            <Row>
+            <div className="Box p-3 d-flex w-100">
+        <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" width={64} height={64} className="d-block rounded-1 mr-3 flex-shrink-0" alt="react logo" />
+        <div className="d-md-flex flex-items-start flex-auto w-100">
+          <div className="flex-auto">
+            <h3 className="mb-1">React</h3>
+            <p>React is an open source JavaScript library used for designing user interfaces.</p>
+            <a className="text-small" href="/topics/react">See topic</a>
+          </div>
+          <div className="d-inline-block js-toggler-container starring-container">
+            {/* '"` */}{/* </textarea></xmp> */}<form className="starred" action="/topics/react/unstar" acceptCharset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" defaultValue="✓" /><input type="hidden" name="_method" defaultValue="delete" /><input type="hidden" name="authenticity_token" defaultValue="5F5JcnD+2OTG9umJ6Hvm89xGc7Q3l6CTx5rDiF32SXBx+Of3jB+Fs/HD7i7rNjXS0k6UwA3Lzm/ClR5aFJ/LRA==" />
+              <input type="hidden" name="context" defaultValue="topic" />
+              <button className="btn btn-sm js-toggler-target" type="submit" value="Unstar" aria-label="Unstar this topic" title="Unstar react" data-ga-click="Topic, click unstar button, action:codesearch#index; text:Unstar">
+                <svg className="octicon octicon-star mr-1" viewBox="0 0 14 16" version="1.1" width={14} height={16} aria-hidden="true"><path fillRule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z" /></svg>Unstar
+              </button>
+            </form>
+            {/* '"` */}{/* </textarea></xmp> */}<form className="unstarred" action="/topics/react/star" acceptCharset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" defaultValue="✓" /><input type="hidden" name="authenticity_token" defaultValue="I2vtMSd1rANkuZsW/lW8XK3Mbk3PN78BP9dA81EvkRP2LtGRUd1QFbES6wPckijak7kfDCdvG5t6XqZyJeceYA==" />
+              <input type="hidden" name="context" defaultValue="topic" />
+              <button className="btn btn-sm js-toggler-target" type="submit" value="Star" aria-label="Star this topic" title="Star react" data-ga-click="Topic, click star button, action:codesearch#index; text:Star">
+                <svg className="octicon octicon-star mr-1" viewBox="0 0 14 16" version="1.1" width={14} height={16} aria-hidden="true"><path fillRule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z" /></svg>Star
+              </button>
+            </form></div>
+        </div>
+      </div>
+            </Row>
+            <Row style={{ backgroundColor: "red" }}>
+                {this.renderSearchResults()}
+            </Row>
+            </Col>
+            </Row>
         )
     }
 }

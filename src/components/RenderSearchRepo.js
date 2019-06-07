b73da47e59dc9c4aaa261752843d2b2bdce1e241
huynhhoangvy <huynh.h.vy@gmail.com>
2019-06-08 02:00:47 +0700

    add 2 left-side table in rendersearchrepo

--- src/components/RenderSearchRepo.js
+++ src/components/RenderSearchRepo.js
@@ -1,7 +1,7 @@
 import Modal from 'react-modal';
 import React from 'react';
 import '../App.css';
-import { Card, Col, Row, Button, ListGroup} from 'react-bootstrap';
+import { Card, Col, Row, Button, ListGroup, Badge} from 'react-bootstrap';
 var moment = require('moment');
 var numeral = require('numeral');
 
@@ -39,30 +39,42 @@ class RenderSearchRepo extends React.Component {
             )
         })
     }
-    render() {
+    render() {                              // have no idea how to apply a certain style to one same tag, ex: listgroup.item
         return (
             <Row>
-                <Col className="col-2 px-2" style={{ backgroundColor: "" }}>
+                <Col className="col-3" style={{padding: "0px 30px"}}>
                 {this.props.totalResult ? 
                 <div>
-                    <ListGroup className="mb-2">
-                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                    <ListGroup className="mb-3" style={{fontSize: "14px"}}>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><strong><a href="#" style={{textDecoration: "none", color: "black"}}>Repositories </a></strong><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>887K</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Code </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>49M+</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Commits </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>49M+</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Issues </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>1M</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Packages </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>2</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Marketplace </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>3</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Topics </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>3K</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Wikis </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>39K</Badge></ListGroup.Item>
+                        <ListGroup.Item style={{display: "flex", justifyContent: "space-between", padding: "8px 10px"}}><a href="#" style={{textDecoration: "none",}}>Users </a><Badge pill variant="secondary" style={{margin: "4px 0 0 4px"}}>19K</Badge></ListGroup.Item>
                     </ListGroup>
                     <ListGroup>
-                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                        <div>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "14px", borderBottom: "none", padding: "16px 16px 8px"}}><strong>Languages</strong></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>JavaScript</a><b>659,086</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>HTML</a><b>30,185</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>TypeScript</a><b>25,866</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>CSS</a><b>22,132</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>Objecti-C</a><b>17,410</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>Java</a><b>11,232</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>Ruby</a><b>8,598</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>Python</a><b>4,008</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderBottom: "none", borderTop: "none", padding: "4px 20px 6px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>PHP</a><b>3,522</b></ListGroup.Item>
+                            <ListGroup.Item style={{display: "flex", justifyContent: "space-between", fontSize: "12px", borderTop: "none", padding: "4px 20px 6px 20px"}}><a href="#" style={{textDecoration: "none", color: "black"}}>C#</a><b>2,393</b></ListGroup.Item>
+                        </div>
                     </ListGroup>
-                    </div>
+                </div>
                     : ""}
                 </Col>
-                <Col className="col-10 px-2">
+                <Col className="col-9 px-2">
                     <Row  className="mx-3">
                     <h4>{this.props.totalResult ? numeral(this.props.totalResult).format('0,0') + " repository results" : ""}</h4>
                     </Row>

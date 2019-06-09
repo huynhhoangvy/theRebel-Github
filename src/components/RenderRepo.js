3174683f7e5d0ba6d64b0057c0f136397db5d26d
Kei <damanhkhoa@gmail.com>
2019-06-09 15:20:54 +0700

    added Huy's work

--- src/components/RenderRepo.js
+++ src/components/RenderRepo.js
@@ -9,6 +9,8 @@ import {
 	Row,
 	Col,
 	Badge,
+	Tab,
+	Tabs,
 } from "react-bootstrap";
 import Modal from 'react-modal'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -17,7 +19,7 @@ const ReactMarkdown = require('react-markdown')
 var moment = require('moment');
 
 
-function Users () {
+function openIssue () {
 	return <h2>Users</h2>;
 }
 
@@ -247,11 +249,31 @@ class RenderRepo extends React.Component {
 				</Modal>
 				<div>Something useful here (navbar for lists of issue) <button onClick={() => this.isOpenIssue()}>New Issue</button> </div>
 				<div>
-					<Modal
+				<Modal
 						isOpen={this.state.isOpenCreateIssue}
 						onRequestClose={() => this.setState({ isOpenCreateIssue: false })}
+						style={{
+							overlay: {
+								backgroundColor: "rgba(244, 247, 252, 0.3)",
+								top: '0%',
+								left: '0%',
+								right: '0%',
+								bottom: '0%',
+								marginRight: '-10%',
+							},
+							content: {
+								top: '15%',
+								left: '10%',
+								right: '10%',
+								bottom: '10%',
+								marginRight: '10%',
+								backgroundColor: "rgba(244, 247, 252, 1)",
+								border: "none",
+							}
+						}}
 					>
 						<div>
+
 							<Form>
 								<Form.Group controlId="exampleForm.ControlInput1">
 									<Form.Label>Title</Form.Label>
@@ -260,14 +282,27 @@ class RenderRepo extends React.Component {
 										onChange={evt => this.props.updateTitle(evt)}
 									/>
 								</Form.Group>
-								<Form.Group controlId="exampleForm.ControlTextarea1">
-									<Form.Label></Form.Label>
-									<Form.Control
-										as="textarea" rows="3" placeholder="leave a comment heres"
-										value={this.props.newCommentIssueCreate}
-										onChange={evt => this.props.updateComment(evt)} />
-									<Button onClick={() => this.handlePostIssue(this.props.newTitleCreate, this.props.newCommentIssueCreate)}>Submit issue</Button>
-								</Form.Group>
+								<Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+									<Tab eventKey="home" title="Comment">
+										<Form.Group controlId="exampleForm.ControlTextarea1">
+											<Form.Label>Comments</Form.Label>
+											<Form.Control
+												size="lg"
+												as="textarea" rows="8" placeholder="leave a comment heres"
+												value={this.props.newCommentIssueCreate}
+												onChange={evt => this.props.updateComment(evt)} />
+										</Form.Group>
+									</Tab>
+									<Tab eventKey="profile" title="Preview">
+										<p>
+											<span style={{ fontSize: "20px" }}>Comment: </span><ReactMarkdown
+											id="hi"
+											source={this.props.newCommentIssueCreate} />
+										</p>
+									</Tab>
+
+								</Tabs>
+								<Button onClick={() => this.handlePostIssue(this.props.newTitleCreate, this.props.newCommentIssueCreate)}>Submit issues</Button>
 							</Form>
 						</div>
 					</Modal>
